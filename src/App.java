463abe8f61ca3ad7c4ddee3149f035e0d5df1061
Dhichana <dhichana@users.noreply.github.com>
2025-05-28 20:46:41 +0530

    Calc

--- src/App.java
+++ src/App.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        System.out.println("------------------");
+        System.out.println(" ----------------");
         System.out.println("| CALCULATOR APP |");
-        System.out.println("------------------");
+        System.out.println(" ----------------");
         System.out.println();
         System.out.println("+===========================+");
         System.out.println("Menu");
